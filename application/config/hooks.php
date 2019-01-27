728879f3b6ba4de618cd5a5b48bf7ba04d725a83
szdn <killrng@outlook.com>
2019-01-28 07:04:37 +0800

    修改/github/test/application/config/hooks.php

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -7,6 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | -------------------------------------------------------------------------
 | This file lets you define "hooks" to extend CI without hacking the core
 | files.  Please see the user guide for info:
+margin: 0
 |
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
