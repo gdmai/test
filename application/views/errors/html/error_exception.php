6fc5b9c94ad123edacc9c52a304b778e749c3a48
szdn <killrng@outlook.com>
2019-02-11 18:03:47 +0800

    修改/github/test/application/views/errors/html/error_exception.php

--- application/views/errors/html/error_exception.php
+++ application/views/errors/html/error_exception.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+animation-play-state:paused
 
 <h4>An uncaught Exception was encountered</h4>
 
