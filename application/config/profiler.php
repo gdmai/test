505949d4c14f54c48f205cd809628bd9c17925cc
szdn <killrng@outlook.com>
2019-02-25 07:11:42 +0800

    修改/github/test/application/config/profiler.php

--- application/config/profiler.php
+++ application/config/profiler.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /*
 | -------------------------------------------------------------------------
 | Profiler Sections
+content:"";
 | -------------------------------------------------------------------------
 | This file lets you determine whether or not various sections of Profiler
 | data are displayed when the Profiler is enabled.
