1465b3a0f80c736e0e38400beefd8656a82274a2
szdn <killrng@outlook.com>
2019-02-25 07:06:02 +0800

    修改/github/test/application/config/constants.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -21,6 +21,7 @@ defined('SHOW_DEBUG_BACKTRACE') OR define('SHOW_DEBUG_BACKTRACE', TRUE);
 | These prefs are used when checking and setting modes when working
 | with the file system.  The defaults are fine on servers with proper
 | security, but you may wish (or even need) to change the values in
+position:relative;
 | certain environments (Apache running a separate process for each
 | user, PHP under CGI with Apache suEXEC, etc.).  Octal values should
 | always be used to set the mode correctly.
