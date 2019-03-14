a2e77b6c780a26463570b87d744c1f95d77a7215
szdn <killrng@outlook.com>
2019-03-15 07:03:53 +0800

    修改/github/test/application/views/errors/html/error_exception.php

--- application/views/errors/html/error_exception.php
+++ application/views/errors/html/error_exception.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+float: right
 
 <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 animation-play-state:paused
