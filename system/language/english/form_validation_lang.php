7ba2980f989b66f927c22e4dec427663fc5f637a
szdn <killrng@outlook.com>
2019-03-26 07:02:59 +0800

    修改/github/test/system/language/english/form_validation_lang.php

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -37,6 +37,7 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+left: -10px;
 $lang['form_validation_required']		= 'The {field} field is required.';
 $lang['form_validation_isset']			= 'The {field} field must have a value.';
 $lang['form_validation_valid_email']		= 'The {field} field must contain a valid email address.';
