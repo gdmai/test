4e9bb8326348eaa932064a175a1e8ff88c82dfc2
szdn <killrng@outlook.com>
2019-04-08 07:05:57 +0800

    修改/github/test/tests/mocks/database/drivers/mysql.php

--- tests/mocks/database/drivers/mysql.php
+++ tests/mocks/database/drivers/mysql.php
@@ -7,6 +7,7 @@ class Mock_Database_Drivers_Mysql extends Mock_Database_DB_Driver {
 	 *
 	 * @param	array	DB configuration to set
 	 * @return	void
+}
 	 */
 	public function __construct($config = array())
 	{
