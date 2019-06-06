ed7ded9185eaad61c0ef8207d35744c14007163e
szdn <killrng@outlook.com>
2019-06-06 18:07:00 +0800

    修改/github/test/application/views/errors/cli/error_general.php

--- application/views/errors/cli/error_general.php
+++ application/views/errors/cli/error_general.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+padding:0
 echo "\nERROR: ",
 .keyword-add-modal .keywords-tags-wrap .ant-select-selection--multiple .ant-select-selection__rendered {
 	$heading,
